2113fd7889a19f628e4d51560a15a54226312d34
Jackson Delahunt <jackson@stemn.com>
2017-07-10 21:19:21 +1000

    release v0.0.2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const root = '/app/website/client';
 
+app.use(morgan('dev'));
 app.use(express.static(root, { maxAge: 31536000000, index: false }));
 app.use('/*', (req, res) => res.sendFile('/index.html', { root, maxAge: 0 }));
 
